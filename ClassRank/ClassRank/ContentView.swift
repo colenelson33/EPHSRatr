7baa8b427739d3db34f1739380f70b3dbae7396e
colenelson33 <colenelson1210@gmail.com>
2022-03-28 13:59:23 -0500

    Test

--- ClassRank/ClassRank/ContentView.swift
+++ ClassRank/ClassRank/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0)
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 230.0/255.0)
 
 
 
