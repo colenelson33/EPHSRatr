86c24bd350d2c369dc1f921372acf32d7a6d9eed
colenelson33 <colenelson1210@gmail.com>
2022-03-29 10:11:28 -0500

    Addedpage etc

---
+++ ClassRank/ClassRank/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  ClassRank
+//
+//  Created by 90304588 on 3/29/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
