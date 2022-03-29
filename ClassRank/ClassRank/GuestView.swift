984ea0af4d2de6053dcd3d6fc5e4c6b000922123
VarunKollipara <60491044+VarunKollipara@users.noreply.github.com>
2022-03-29 11:39:08 -0500

    added new button style

--- ClassRank/ClassRank/GuestView.swift
+++ ClassRank/ClassRank/GuestView.swift
@@ -11,30 +11,47 @@ struct GuestView: View {
     var body: some View {
         NavigationView{
             VStack{
+                
                 Button {
                     print("Clicked")
-                    
                 } label: {
                     Text("Math")
-                    
-                }.buttonStyle(GrowingBlockButton())
-                    
-                
-                
-                
+                }.buttonStyle(GrowingBlockRedButton())
+                    .position(x: 110, y: -20)
+               
+                Button {
+                    print("Clicked")
+                } label: {
+                    Text("English")
+                }.buttonStyle(GrowingBlockWhiteButton())
+                    .position(x: 110, y: -220)
                 
             }
         }
     }
 }
-struct GrowingBlockButton: ButtonStyle {
+struct GrowingBlockRedButton: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
-            .frame(width: 150, height: 150)
+            .frame(width: 150, height: 100)
             .background(Color.red)
             .foregroundColor(.white)
             .clipShape(Capsule())
+            .scaleEffect(configuration.isPressed ? 1.2 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct GrowingBlockWhiteButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(width: 150, height: 100)
+            .background(Color.white)
+            .foregroundColor(.red)
+            .overlay(Capsule(style: .continuous)
+                .stroke(Color.red, lineWidth: 5))
             .scaleEffect(configuration.isPressed ? 1.2 : 1)
             .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
     }
