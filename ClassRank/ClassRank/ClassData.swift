349208c5bc0a26f92a15cad37b6504b17e908742
colenelson33 <colenelson1210@gmail.com>
2022-04-22 10:41:02 -0500

    added class

--- ClassRank/ClassRank/ClassData.swift
+++ ClassRank/ClassRank/ClassData.swift
@@ -30,7 +30,7 @@ extension GlobalVar{
     
     static let ScienceClasses = [apstats, apstats, apstats]
     
-    static let 
+    static let advHeritageSpanish = ClassData(className: "Adv Comp&Comm Heritage Spanish A&B", Teacher: "", credits: 2, preR: "Spanish spoken in the home", description: "This course is designed for students who speak Spanish in the home and/or are fluent Spanish speakers. In this course, students are able to develop their linguistic ability in association with their written and comprehensive skills. Furthermore, emphasis is also placed on the subjects of communication and Latin American history. This course prepares students for the Bilingual Seal exam, in which they can earn college credit.")
     
     static let SocialClasses = [apstats, apstats, apstats]
     
